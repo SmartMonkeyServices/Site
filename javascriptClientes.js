0fa94399d54b1d7831b671ab45ddba97233fca9a
Wesley Alves de Albuquerque <wealbuquerque@yahoo.com.br>
2024-02-27 23:19:25 -0300

    Ajustado host

--- javascriptClientes.js
+++ javascriptClientes.js
@@ -7,7 +7,7 @@ const uf = document.getElementById('estado');
 const pais = document.getElementById('pais');
 var fantasia = document.getElementById("divfantasia");
 var divResultado = document.getElementById("divResultado");
-const hostAPI = "http://127.0.0.1:5000";
+const hostAPI = "api-production-b825.up.railway.app";
 
 
 function cepOnchange(){
